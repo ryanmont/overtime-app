094d8308c23aa61253f29bad55bde744d48f1310
Ryan Mont <ryanmont@gmail.com>
2017-03-22 10:15:29 -0600

    refactor

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'navigate' do
+ describe 'index' do
+  it 'can be reached successfully' do
+    visit posts_path
+    expect(page.status_code). to eq(200)
+  end
+
+  it 'has a title of Posts' do
+    visit posts_path
+    expect(page). to have_content (/Posts/)
+    end
+  end
+describe 'creation' do
+  before do 
+    visit ne_post_path
+  end
+
+  it 'has a form that can be reached'
+  
+  expect (page.status_code).to eq(200)
+    end
+
+  it 'can be created from new form page' do
+  fill_in 'post[date]', with: Date.today
+  fill_in 'post[rationale]', with: "Some rationale"
+  click_on "Save"
+
+  expect(page).to have_content("Some rationale")
+
+    end
+  end
+end
