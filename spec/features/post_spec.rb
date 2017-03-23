54aae264d276499d18b7daec486d9753b5835338
Ryan Mont <ryanmont@gmail.com>
2017-03-23 15:41:41 -0600

    more jordan crap

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -26,16 +26,25 @@ describe 'navigate' do
       expect(page).to have_content(/Rationale|content/)
     end
   end
-  
-describe 'new' do
-  it 'has a link from the homepage' do
-    visit root_path
 
-    click_link("new_post_from_nav")
-    expect(page.status_code).to eq(200)
+  describe 'new' do
+    it 'has a link from the homepage' do
+      visit root_path
+
+      click_link("new_post_from_nav")
+      expect(page.status_code).to eq(200)
+    end
   end
-end
 
+  describe 'delete' do
+    it 'can be deleted' do
+      @post = FactoryGirl.create(:post)
+      visit posts_path
+
+      click_link("delete_post_#{@post.id}_from_index")
+      expect(page.status_code).to eq(200)
+    end
+  end
 
   describe 'creation' do
     before do
