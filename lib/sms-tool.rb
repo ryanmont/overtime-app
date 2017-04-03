90124a4e265fe81899b0d693cb7b1e9b3baf5e29
Ryan Mont <ryanmont@gmail.com>
2017-04-03 16:45:39 -0600

    sending texts from twilio

--- lib/sms-tool.rb
+++ lib/sms-tool.rb
@@ -1,6 +1,20 @@
+
 module SmsTool
+  account_sid = ENV['AC6c50b292f7db30040c15ee1815031613']
+  auth_token = ENV['1c3977f73d13328e2ca64f7106085716']
+
+  @client = Twilio::REST::Client.new account_sid, auth_token
+
   def self.send_sms(number:, message:)
-    puts "Sending SMS..."
-    puts "#{message} to #{number}"
+    @client.messages.create(
+      from: ENV['18018452384'],
+      to: "+1#{number}",
+      body: "#{message}"
+      )
+
+     
+    
   end
 end
+
+
